5010c9c831df4028598c183782cda228ba9ad794
Sahaj Amatya <sahajamatya1@gmail.com>
2020-02-03 17:29:45 -0600

    “commit_via_batch_file”

--- js/app.js
+++ js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"//changed this
+        "value": "#000000"//changed this
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#ffffff"//changed this
+          "color": "#000000"//changed this
         },
         "polygon": {
           "nb_sides": 10
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",//changed this
+        "color": "#000000",//changed this
         "opacity": 0.4,
         "width": 1
       },
