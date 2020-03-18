cfeeb75b5e237877aa2a6ef15156ef11d56cf355
Sahaj Amatya <sahajamatya1@gmail.com>
2020-03-17 23:01:33 -0500

    darkMode fiddle

--- functions.js
+++ functions.js
@@ -1,27 +1,43 @@
-$(function() {
+$(function () {
+  $(".upcomingPosts").hide();
+  $(".darkModeMsg").hide();
+  $(".pubPosts").click(function () {
+    $(".upPosts").css("opacity", "0.5");
+    $(".pubPosts").css("opacity", "1");
     $(".upcomingPosts").hide();
-    $(".darkModeMsg").hide();
-    $(".pubPosts").click(function () {
-      $(".upPosts").css("opacity", "0.5");
-      $(".pubPosts").css("opacity", "1");
-      $(".upcomingPosts").hide();
-      $(".publishedPosts").fadeIn(500);
-    });
-    $(".upPosts").click(function () {
-      $(".pubPosts").css("opacity", "0.5");
-      $(".upPosts").css("opacity", "1");
-      $(".upcomingPosts").fadeIn(500);
-      $(".publishedPosts").hide();
-    });
-    $(".slider").click(function () {
-      // if ($(".style").attr("href") == "style.css") {
-      //   $(".style").attr("href", "darkMode.css");
-      // } else {
-      //   $(".style").attr("href", "style.css");  
-      // }
-      $(".darkModeMsg").show(500);
-      setTimeout(function () {
-        $(".darkModeMsg").hide(500);
-      }, 2000);
-    });
+    $(".publishedPosts").fadeIn(500);
   });
+  $(".upPosts").click(function () {
+    $(".pubPosts").css("opacity", "0.5");
+    $(".upPosts").css("opacity", "1");
+    $(".upcomingPosts").fadeIn(500);
+    $(".publishedPosts").hide();
+  });
+  var mode = "light";
+  $(".slider").click(function () {
+    // if ($(".style").attr("href") == "style.css" || mode == "dark") {
+    //   mode = "dark";
+    //   $(".style").attr("href", "darkMode.css");
+    //   $("a").each(function () {
+    //     var $this = $(this);
+    //     var _href = $this.attr("href");
+    //     $this.attr("href", _href + '?mode=' + mode);
+    //   });
+    // } else {
+    //   mode = "light";
+    //   $(".style").attr("href", "style.css");
+    //   $("a").each(function () {
+    //     var $this = $(this);
+    //     var _href = $this.attr("href");
+    //     $this.attr("href", _href + '?mode=' + mode);
+    //   });
+    // }
+
+    $(".darkModeMsg").show(500);
+    setTimeout(function () {
+      $(".darkModeMsg").hide(500);
+    }, 2000);
+  });
+
+
+});
