2f190ae25d9e6427dcac5ebc76e6d5c642af2e75
Sahaj Amatya <sahajamatya1@gmail.com>
2020-03-19 01:12:31 -0500

    “commit_via_batch_file”

--- functions.js
+++ functions.js
@@ -13,79 +13,59 @@ $(function () {
     $(".upcomingPosts").fadeIn(500);
     $(".publishedPosts").hide();
   });
-  var urlMode = new URLSearchParams(window.location.search);
-  var mode = urlMode.toString();
-  if (mode != "") {
-    $(".darkModeMsg").hide();
-    // console.log("The mode is: "+mode);
-    if (mode.indexOf("ciHvhtdD=")!=-1) {
-      $('.darkModeSelector').prop('checked', true);
-      $(".style").attr("href", "../darkMode.css");
-      //console.log("Also, the style sheet is: " + $(".style").attr("href"));
-      $("a").each(function () {
-        var $this = $(this);
-        var _href = $this.attr("href");
-        $this.attr("href", _href + '?' + mode);
-      });
-    } else {
-      mode = "yFpKKwpf";
-      $(".style").attr("href", "../style.css");
-      $("a").each(function () {
-        var $this = $(this);
-        var _href = $this.attr("href");
-        $this.attr("href", _href + '?' + mode);
-      });
-    }
-  }
-  var is_it_checked = $(".darkModeSelector").prop("checked");
-    if (is_it_checked==true) {
-      mode = "ciHvhtdD";
-      $(".style").attr("href", "darkMode.css");
-      $(".modeIcon").attr("src", "img/lightmode.png");
-      $(".scrollup").attr("src", "img/scrollupLight.png");
-      $("a").each(function () {
-        var $this = $(this);
-        var _href = $this.attr("href");
-        $this.attr("href", _href + '?' + mode);
-      });
-    } else {
-      mode = "yFpKKwpf";
-      $(".style").attr("href", "style.css");
-      $(".modeIcon").attr("src", "img/darkmode.png");
-      $(".scrollup").attr("src", "img/scrollup.png");
-      $("a").each(function () {
-        var $this = $(this);
-        var _href = $this.attr("href");
-        $this.attr("href", _href + '?' + mode);
-      });
-    }
+  // var urlMode = new URLSearchParams(window.location.search);
+  // var mode = urlMode.toString();
+  // Cookies.set('dark','');
+  // var darkMode = Cookies.get('dark');
+  // var is_it_checked = $(".darkModeSelector").prop("checked");
+  //   if (is_it_checked==true) {
+  //     mode = "ciHvhtdD";
+  //     $(".style").attr("href", "darkMode.css");
+  //     $(".modeIcon").attr("src", "img/lightmode.png");
+  //     $(".scrollup").attr("src", "img/scrollupLight.png");
+  //     $("a").each(function () {
+  //       var $this = $(this);
+  //       var _href = $this.attr("href");
+  //       $this.attr("href", _href + '?' + mode);
+  //     });
+  //   } else {
+  //     mode = "yFpKKwpf";
+  //     $(".style").attr("href", "style.css");
+  //     $(".modeIcon").attr("src", "img/darkmode.png");
+  //     $(".scrollup").attr("src", "img/scrollup.png");
+  //     $("a").each(function () {
+  //       var $this = $(this);
+  //       var _href = $this.attr("href");
+  //       $this.attr("href", _href + '?' + mode);
+  //     });
+  //   }
   $(".darkModeSelector").on("change",function () {
-    var is_it_checked = $(this).prop("checked");
-    if (is_it_checked==true) {
-      mode = "ciHvhtdD";
-      $(".style").attr("href", "darkMode.css");
-      $(".modeIcon").attr("src", "img/lightmode.png");
-      $(".scrollup").attr("src", "img/scrollupLight.png");
-      $("a").each(function () {
-        var $this = $(this);
-        var _href = $this.attr("href");
-        var indexOfQ = _href.indexOf("?");
-        _href=_href.substr(0,indexOfQ);
-        $this.attr("href", _href + '?' + mode);
-      });
-    } else {
-      mode = "yFpKKwpf";
-      $(".style").attr("href", "style.css");
-      $(".modeIcon").attr("src", "img/darkmode.png");
-      $(".scrollup").attr("src", "img/scrollup.png");
-      $("a").each(function () {
-        var $this = $(this);
-        var _href = $this.attr("href");
-        var indexOfQ = _href.indexOf("?");
-        _href=_href.substr(0,indexOfQ);
-        $this.attr("href", _href + '?' + mode);
-      });
-    }
+  //   var is_it_checked = $(this).prop("checked");
+  //   if (is_it_checked==true) {
+  //     mode = "ciHvhtdD";
+  //     $(".style").attr("href", "darkMode.css");
+  //     $(".modeIcon").attr("src", "img/lightmode.png");
+  //     $(".scrollup").attr("src", "img/scrollupLight.png");
+  //     $("a").each(function () {
+  //       var $this = $(this);
+  //       var _href = $this.attr("href");
+  //       var indexOfQ = _href.indexOf("?");
+  //       _href=_href.substr(0,indexOfQ);
+  //       $this.attr("href", _href + '?' + mode);
+  //     });
+  //   } else {
+  //     mode = "yFpKKwpf";
+  //     $(".style").attr("href", "style.css");
+  //     $(".modeIcon").attr("src", "img/darkmode.png");
+  //     $(".scrollup").attr("src", "img/scrollup.png");
+  //     $("a").each(function () {
+  //       var $this = $(this);
+  //       var _href = $this.attr("href");
+  //       var indexOfQ = _href.indexOf("?");
+  //       _href=_href.substr(0,indexOfQ);
+  //       $this.attr("href", _href + '?' + mode);
+  //     });
+  //   }
 
     $(".darkModeMsg").show(500);
     setTimeout(function () {
